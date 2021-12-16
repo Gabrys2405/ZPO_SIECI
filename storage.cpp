1a411de056c362edddaa4283585d460f7c70c0cc
Gabriel Maj <majgabriel@student.agh.edu.pl>
2021-12-16 14:18:40 +0100

    Małe definicje klas

--- storage.cpp
+++ storage.cpp
@@ -4,3 +4,29 @@
 
 
 #include "storage.hpp"
+
+pack IPackageQueue::pop() {
+    throw;
+    //return pack();
+}
+
+//int IPackageQueue::get_queue_type() {
+  //  return 0;
+}
+
+void IPackageStockpile::push() {
+    throw;
+
+}
+
+std::size_t IPackageStockpile::size() {
+    throw;
+}
+
+bool IPackageStockpile::empty() {
+    throw;
+}
+
+void IPackageStockpile::push(Package &&) {
+    throw;
+}
--- storage.hpp
+++ storage.hpp
@@ -9,7 +9,7 @@
 #include <list>
 
 
-using element_id = unsigned int; // Dla uproszczenia
+using element_id = unsigned int; // Dla uproszczenia(<<primitive>> )
 using pack = std::list<int>; //Wydaje mi się że ta kolejka elementów ma byc kolejką ID ale pewności nie mam
 
 class Package{
@@ -17,11 +17,38 @@ public:
     Package(const element_id id): ElementID(id) {}
 
     element_id get_id() const {return ElementID;};
-    ~ Package() = default;
+    ~ Package() = default;//????
 private:
     element_id ElementID;
 
 
+
+};
+
+
+
+class IPackageStockpile{
+public:
+    using const_iterator = std::list<Package>::const_iterator;
+
+
+    virtual void push(Package&&);
+    virtual void push();
+    virtual std::size_t size();
+    virtual bool empty();
+    virtual ~IPackageStockpile() = default;//????
+private:
+
+
+
+
+};
+class IPackageQueue : public IPackageStockpile{  //?????
+public:
+    virtual pack pop();
+    //virtual  get_queue_type();
+private:
+pack Package;
 };
 
 class PackageQueue{
@@ -31,24 +58,9 @@ public:
 private:
 
 };
-class IPackageQueue{
-public:
-    virtual pack pop(){return Package;};
-
-private:
-pack Package;
-};
-
-
-class IPackageStockpile{
-public:
-
-private:
 
 
 
-
-};
 
 
 
