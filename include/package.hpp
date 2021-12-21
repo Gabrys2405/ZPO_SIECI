6db7397edddbd4502c5adcdc19efeccc883256f7
Gabriel Maj <majgabriel@student.agh.edu.pl>
2021-12-21 16:38:58 +0100

    Zrobione konstruktory

--- include/package.hpp
+++ include/package.hpp
@@ -21,6 +21,7 @@ public:
                 }
             }
             _id = max_id + 1;
+            assigned_IDs.insert(_id);
             //TODO Trzeba znaleźć największe ID w assigned_IDs i dodać do niego 1 a następnie ustawić jako id!!!
         }
         else if(!freed_IDs.empty()){
@@ -46,7 +47,7 @@ public:
         freed_IDs.insert(_id);
     };//TODO Kiedy usuwamy obiekt klasy Package jego ID ląduje w zbiorze freed_IDs!!!
 private:
-    ElementID _id;
+    ElementID _id = 0;
     static std::set<ElementID> assigned_IDs; // przydzielone obecnie ID
     static std::set<ElementID> freed_IDs; // kiedyś użyte ale obecnie zwolnione ID
 
--- main.cpp
+++ main.cpp
@@ -5,3 +5,21 @@
 #include "include/storage_types.hpp"
 #include "src/package.cpp"
 #include "src/storage_types.cpp"
+#include "include/package.hpp"
+
+#include <iostream>
+int main(){
+    Package aPackage;
+    std::cout<< aPackage.get_id()<<std::endl;
+    Package aPackage1;
+    std::cout <<aPackage1.get_id()<<std::endl;
+    Package aPackage2;
+    std::cout << aPackage2.get_id()<<std::endl;
+    Package aPackage3;
+    std::cout << aPackage3.get_id()<<std::endl;
+
+
+    PackageQueue packageQueue(PackageQueueType::FIFO);
+    return 0;
+
+}
